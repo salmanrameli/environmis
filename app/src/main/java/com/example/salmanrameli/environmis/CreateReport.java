af21df05690bd3b694185bce56cbb322d3c1043c
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-02 19:49:10 +0700

    add feature to get current location latitude and longitude, location latitude and longitude input value now got from pressing getLocationButton

--- app/src/main/java/com/example/salmanrameli/environmis/CreateReport.java
+++ app/src/main/java/com/example/salmanrameli/environmis/CreateReport.java
@@ -1,13 +1,17 @@
 package com.example.salmanrameli.environmis;
 
 import android.app.DatePickerDialog;
+import android.Manifest;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.mock.MockPackageManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -24,18 +28,56 @@ public class CreateReport extends AppCompatActivity {
     EditText report_date_text;
     EditText report_result_text;
     Calendar calendar = Calendar.getInstance();
+    double latitude_val;
+    double longitude_val;
+
+    Button getLocationButton;
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String permission = Manifest.permission.ACCESS_FINE_LOCATION;
+    GPSTracker gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_report);
 
-        reportDbHelper = new ReportDbHelper(this);
-
         report_location_latitude_text = (EditText) findViewById(R.id.report_location_latitude);
         report_location_longitude_text = (EditText) findViewById(R.id.report_location_longitude);
         report_date_text = (EditText) findViewById(R.id.report_date_);
         report_result_text = (EditText) findViewById(R.id.report_result_);
+
+        try {
+            if(ActivityCompat.checkSelfPermission(this, permission)
+                    != MockPackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this, new String[]{permission}, REQUEST_CODE_PERMISSION);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        getLocationButton = (Button) findViewById(R.id.getLocationButton);
+
+        getLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gps = new GPSTracker(CreateReport.this);
+
+                if(gps.canGetLocation()) {
+                    latitude_val = gps.getLatitude();
+                    longitude_val = gps.getLongitude();
+
+                    report_location_latitude_text.setText(String.valueOf(latitude_val));
+                    report_location_longitude_text.setText(String.valueOf(longitude_val));
+
+//                    Toast.makeText(getApplicationContext(), "Latitude: " + latitude_val + "\nLongitude: " + longitude_val, Toast.LENGTH_LONG).show();
+                } else {
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+
+        reportDbHelper = new ReportDbHelper(this);
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
