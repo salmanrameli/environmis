9e3ed1c0e57598a3a57134d672b853748f61c21e
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-04 14:28:50 +0700

    add sign out mechanism

--- app/src/main/java/com/example/salmanrameli/environmis/Home.java
+++ app/src/main/java/com/example/salmanrameli/environmis/Home.java
@@ -1,6 +1,8 @@
 package com.example.salmanrameli.environmis;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(SignIn.MyPREFERENCES, Context.MODE_PRIVATE);
     }
 
     public void createReport(View view)
@@ -24,6 +28,20 @@ public class Home extends AppCompatActivity {
     {
         Intent intent = new Intent(this, IndexReport.class);
 
+        startActivity(intent);
+    }
+
+    public void signOutButtonOnClick(View view)
+    {
+        SharedPreferences sharedPreferences = getSharedPreferences(SignIn.MyPREFERENCES, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.clear();
+        editor.apply();
+
+        Intent intent = new Intent(Home.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 }
