b2bb7e5390aca414b2a9bf57505e686bca176b28
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-06 15:36:04 +0700

    add activity assign task for validator role account

--- app/src/main/java/com/example/salmanrameli/environmis/AssignTask.java
+++ app/src/main/java/com/example/salmanrameli/environmis/AssignTask.java
@@ -1,5 +1,6 @@
 package com.example.salmanrameli.environmis;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +10,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.salmanrameli.db.AssignTaskContract;
+import com.example.salmanrameli.db.AssignTaskDbHelper;
 import com.example.salmanrameli.db.StaffContract;
 import com.example.salmanrameli.db.StaffDbHelper;
 
@@ -18,9 +22,11 @@ import java.util.List;
 
 public class AssignTask extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private StaffDbHelper staffDbHelper;
+    private AssignTaskDbHelper assignTaskDbHelper;
     EditText location_latitude;
     EditText location_longitude;
     Spinner spinner;
+    String selected_staff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,8 @@ public class AssignTask extends AppCompatActivity implements AdapterView.OnItemS
 
         SQLiteDatabase db = staffDbHelper.getReadableDatabase();
 
+        location_latitude = (EditText) findViewById(R.id.assignTaskLocationLatitude);
+        location_longitude = (EditText) findViewById(R.id.assignTaskLocationLongitude);
         spinner = (Spinner) findViewById(R.id.spinner);
 
         spinner.setOnItemSelectedListener(this);
@@ -55,11 +63,38 @@ public class AssignTask extends AppCompatActivity implements AdapterView.OnItemS
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+        selected_staff = parent.getItemAtPosition(position).toString();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void assignTaskButtonOnClick(View view) {
+        String location_lat = String.valueOf(location_latitude.getText().toString());
+        String location_long = String.valueOf(location_longitude.getText().toString());
+
+        assignTaskDbHelper = new AssignTaskDbHelper(this);
+
+        SQLiteDatabase db = assignTaskDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(AssignTaskContract.AssignTaskEntry.COL_LOCATION_LATITUDE, location_lat);
+        values.put(AssignTaskContract.AssignTaskEntry.COL_LOCATION_LONGITUDE, location_long);
+        values.put(AssignTaskContract.AssignTaskEntry.COL_STAFF_ID, selected_staff);
+        values.put(AssignTaskContract.AssignTaskEntry.COL_IS_DONE, "false");
+
+        db.insertWithOnConflict(AssignTaskContract.AssignTaskEntry.TABLE,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+
+        db.close();
+
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+
+        finish();
     }
 }
