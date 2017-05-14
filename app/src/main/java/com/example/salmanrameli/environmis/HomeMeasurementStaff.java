5a29d20b827b5f6fe220332509cc93f280a3692c
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-14 12:47:17 +0700

    get firebase authentication instance

--- app/src/main/java/com/example/salmanrameli/environmis/HomeMeasurementStaff.java
+++ app/src/main/java/com/example/salmanrameli/environmis/HomeMeasurementStaff.java
@@ -1,21 +1,25 @@
 package com.example.salmanrameli.environmis;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class HomeMeasurementStaff extends AppCompatActivity {
     String _id;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        _id = getIntent().getStringExtra("user_id_session");
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
     }
 
     public void createReport(View view)
@@ -37,12 +41,7 @@ public class HomeMeasurementStaff extends AppCompatActivity {
 
     public void signOutButtonOnClick(View view)
     {
-        SharedPreferences sharedPreferences = getSharedPreferences(SignIn.MyPREFERENCES, Context.MODE_PRIVATE);
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        editor.clear();
-        editor.apply();
+        firebaseAuth.signOut();
 
         Intent intent = new Intent(HomeMeasurementStaff.this, MainActivity.class);
 
--- app/src/main/java/com/example/salmanrameli/environmis/HomeValidatorStaff.java
+++ app/src/main/java/com/example/salmanrameli/environmis/HomeValidatorStaff.java
@@ -1,18 +1,25 @@
 package com.example.salmanrameli.environmis;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
 public class HomeValidatorStaff extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_validator_staff);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
     }
 
     public void viewReportButtonOnClick(View view)
@@ -31,12 +38,7 @@ public class HomeValidatorStaff extends AppCompatActivity {
 
     public void validatorSignOutButtonOnClick(View view)
     {
-        SharedPreferences sharedPreferences = getSharedPreferences(SignIn.MyPREFERENCES, Context.MODE_PRIVATE);
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        editor.clear();
-        editor.apply();
+        firebaseAuth.signOut();
 
         Intent intent = new Intent(HomeValidatorStaff.this, MainActivity.class);
 
