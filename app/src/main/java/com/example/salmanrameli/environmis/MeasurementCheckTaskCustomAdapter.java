ac4e54ca1d4ec73ab6a0e8c0c15bab1e1ca8534d
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-18 11:04:20 +0700

    calculate distance between current location and supposed measurement location, then set whether button do this task is clickable or not based on the distance

--- app/src/main/java/com/example/salmanrameli/environmis/MeasurementCheckTaskCustomAdapter.java
+++ app/src/main/java/com/example/salmanrameli/environmis/MeasurementCheckTaskCustomAdapter.java
@@ -7,16 +7,35 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 class MeasurementCheckTaskCustomAdapter extends BaseAdapter {
+    private GPSTracker gps;
+
     private Context context;
     private ArrayList<TaskToDo> taskToDos;
+
+    private double distance;
+    private double current_latitude_val;
+    private double current_longitude_val;
+    private double supposed_latitude_val;
+    private double supposed_longitude_val;
 
     MeasurementCheckTaskCustomAdapter(Context context, ArrayList<TaskToDo> taskToDos) {
         this.context = context;
         this.taskToDos = taskToDos;
+
+        gps = new GPSTracker(context);
+
+        if(gps.canGetLocation()) {
+            current_latitude_val = gps.getLatitude();
+            current_longitude_val = gps.getLongitude();
+        }
+        else {
+            Toast.makeText(context, "Can't get location", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -49,6 +68,40 @@ class MeasurementCheckTaskCustomAdapter extends BaseAdapter {
         latitude_text.setText(taskToDo.getLocation_latitude());
         longitude_text.setText(taskToDo.getLocation_longitude());
 
+        supposed_latitude_val = Double.parseDouble(taskToDo.getLocation_latitude());
+        supposed_longitude_val = Double.parseDouble(taskToDo.getLocation_longitude());
+
+        distance = getDistance(supposed_latitude_val, supposed_longitude_val, current_latitude_val, current_longitude_val);
+
+        if(distance > 1) {
+            do_task.setEnabled(false);
+        }
+        else {
+            do_task.setEnabled(true);
+        }
+
         return convertView;
+    }
+
+    private double getDistance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2))
+                + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+        return (dist);
+    }
+
+    private double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    private double rad2deg(double rad) {
+        return (rad * 180.0 / Math.PI);
     }
 }
