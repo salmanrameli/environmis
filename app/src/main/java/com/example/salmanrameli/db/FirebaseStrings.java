f3d5396528e3a6b839d89bb96b36b27489adc9c8
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-18 19:37:32 +0700

    strings used in the firebase

---
+++ app/src/main/java/com/example/salmanrameli/db/FirebaseStrings.java
@@ -0,0 +1,29 @@
+package com.example.salmanrameli.db;
+
+public class FirebaseStrings {
+    public static final String USERS = "users";
+
+    public static final String TODO = "todo";
+
+    public static final String REPORTS = "reports";
+
+    public static final String USERNAME = "username";
+
+    public static final String ROLE = "role";
+
+    public static final String STAFF_ID = "staff_id";
+
+    public static final String LOCATION_LATITUDE = "location_latitude";
+
+    public static final String LOCATION_LONGITUDE = "location_longitude";
+
+    public static final String MEASUREMENT_DATE = "measurement_date";
+
+    public static final String MEASUREMENT_RESULT = "measurement_result";
+
+    public static final String IS_DONE = "is_done";
+
+    public static final String MEASUREMENT_ROLE = "measurement";
+
+    public static final String VALIDATOR_ROLE = "validator";
+}
