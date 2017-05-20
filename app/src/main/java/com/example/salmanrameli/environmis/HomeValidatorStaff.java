3f8964701b1528a797eac34ab3d1618c5b4b21cf
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-20 10:53:43 +0700

    add hardcoded location coordinate to firebase

--- app/src/main/java/com/example/salmanrameli/environmis/HomeValidatorStaff.java
+++ app/src/main/java/com/example/salmanrameli/environmis/HomeValidatorStaff.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.salmanrameli.db.FirebaseStrings;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeValidatorStaff extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,61 @@ public class HomeValidatorStaff extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+//        Map<String, String> map = new HashMap<>();
+//
+//        map.put(FirebaseStrings.LOCATION_NAME, "Teknik Informatika ITS");
+//        map.put(FirebaseStrings.LOCATION_LATITUDE, "-7.279581");
+//        map.put(FirebaseStrings.LOCATION_LONGITUDE, "112.797595");
+//
+//        DatabaseReference _id = databaseReference.child(FirebaseStrings.LOCATION).push();
+//        String key_id = _id.getKey();
+//
+//        map.put("location_key", key_id);
+//
+//        _id.setValue(map);
+//
+//        map = new HashMap<>();
+//
+//        map.put(FirebaseStrings.LOCATION_NAME, "Kantin Pusat ITS");
+//        map.put(FirebaseStrings.LOCATION_LATITUDE, "-7.284059");
+//        map.put(FirebaseStrings.LOCATION_LONGITUDE, "112.793980");
+//
+//        _id = databaseReference.child(FirebaseStrings.LOCATION).push();
+//        key_id = _id.getKey();
+//
+//        map.put("location_key", key_id);
+//
+//        _id.setValue(map);
+//
+//        map = new HashMap<>();
+//
+//        map.put(FirebaseStrings.LOCATION_NAME, "SMP Negeri 6 Surabaya");
+//        map.put(FirebaseStrings.LOCATION_LATITUDE, "-7.273889");
+//        map.put(FirebaseStrings.LOCATION_LONGITUDE, "112.748548");
+//
+//        _id = databaseReference.child(FirebaseStrings.LOCATION).push();
+//        key_id = _id.getKey();
+//
+//        map.put("location_key", key_id);
+//
+//        _id.setValue(map);
+//
+//        map = new HashMap<>();
+//
+//        map.put(FirebaseStrings.LOCATION_NAME, "SMA Negeri 2 Surabaya");
+//        map.put(FirebaseStrings.LOCATION_LATITUDE, "-7.256216");
+//        map.put(FirebaseStrings.LOCATION_LONGITUDE, "112.749536");
+//
+//        _id = databaseReference.child(FirebaseStrings.LOCATION).push();
+//        key_id = _id.getKey();
+//
+//        map.put("location_key", key_id);
+//
+//        _id.setValue(map);
     }
 
     public void viewReportButtonOnClick(View view)
