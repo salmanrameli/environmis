8b241cea7e06f021e07644e839695e2ee189106c
Anugra Pratama Rameli <salmanrameli@hotmail.com>
2017-05-22 19:40:18 +0700

    location chosen from maps instead of spinner

--- app/src/main/java/com/example/salmanrameli/environmis/AssignTask.java
+++ app/src/main/java/com/example/salmanrameli/environmis/AssignTask.java
@@ -1,5 +1,11 @@
 package com.example.salmanrameli.environmis;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +15,19 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.salmanrameli.db.FirebaseStrings;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,19 +41,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class AssignTask extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    Spinner spinner;
-    Spinner spinnerLocation;
+public class AssignTask extends AppCompatActivity implements AdapterView.OnItemSelectedListener,
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
+    Spinner spinner;
     String selected_staff;
-    String selected_location;
-    String location_latitude;
-    String location_longitude;
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+
+    MarkerOptions markerOptions;
+    GoogleMap mGoogleMap;
+    SupportMapFragment mapFrag;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    boolean firsttime=false;
+    Marker mymarker;
+    LatLng myloc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,72 +104,93 @@ public class AssignTask extends AppCompatActivity implements AdapterView.OnItemS
             }
         });
 
-        databaseReference.child(FirebaseStrings.LOCATION).addValueEventListener(new ValueEventListener() {
+        mapFrag = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFrag.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            buildGoogleApiClient();
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+
+        mGoogleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                final List<String> location_names = new ArrayList<String>();
-
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Location location = snapshot.getValue(Location.class);
-
-                    String location_name = location.getLocation_name();
-
-                    location_names.add(location_name);
+            public void onMapLongClick(LatLng arg0) {
+                // TODO Auto-generated method stub
+                if(mymarker != null) {
+                    mymarker.remove();
                 }
 
-                spinnerLocation = (Spinner) findViewById(R.id.spinnerLocationName);
-                spinnerLocation.setOnItemSelectedListener(AssignTask.this);
+                markerOptions = new MarkerOptions();
 
-                ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(AssignTask.this, android.R.layout.simple_spinner_dropdown_item, location_names);
-                arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                markerOptions.position(arg0);
+                markerOptions.title(arg0.latitude + " , " + arg0.longitude);
 
-                spinnerLocation.setAdapter(arrayAdapter1);
-            }
+                mymarker = mGoogleMap.addMarker(markerOptions);
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+                myloc=arg0;
             }
         });
     }
 
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        if(!firsttime){
+            CameraUpdate cu = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude()));
+            mGoogleMap.moveCamera(cu);
+            firsttime=true;
+        }
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Spinner parentSpinner = (Spinner) parent;
-
-        switch(parentSpinner.getId()) {
-            case R.id.spinner:
-                selected_staff = parent.getItemAtPosition(position).toString();
-
-                break;
-
-            case R.id.spinnerLocationName:
-                selected_location = parent.getItemAtPosition(position).toString();
-
-                databaseReference.child(FirebaseStrings.LOCATION).addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            Location location = snapshot.getValue(Location.class);
-
-                            String _location_name = location.getLocation_name();
-
-                            if(_location_name.equals(selected_location)) {
-                                location_latitude = location.getLocation_latitude();
-                                location_longitude = location.getLocation_longitude();
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-                break;
-        }
-
+        selected_staff = parent.getItemAtPosition(position).toString();
     }
 
     @Override
@@ -150,10 +199,13 @@ public class AssignTask extends AppCompatActivity implements AdapterView.OnItemS
     }
 
     public void assignTaskButtonOnClick(View view) {
+        String location_lat = String.valueOf(myloc.latitude);
+        String location_long = String.valueOf(myloc.longitude);
+
         Map<String, String> map = new HashMap<>();
 
-        map.put(FirebaseStrings.LOCATION_LATITUDE, location_latitude);
-        map.put(FirebaseStrings.LOCATION_LONGITUDE, location_longitude);
+        map.put(FirebaseStrings.LOCATION_LATITUDE, location_lat);
+        map.put(FirebaseStrings.LOCATION_LONGITUDE, location_long);
         map.put(FirebaseStrings.STAFF_ID, selected_staff);
         map.put(FirebaseStrings.IS_DONE, "false");
 
